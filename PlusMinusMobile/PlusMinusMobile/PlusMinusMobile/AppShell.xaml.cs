870cc7406a01dfc1bf5231dff41eb2d3b532908a
DiabloCode66 <heorhii.maslov@nure.ua>
2022-06-18 22:54:23 +0300

    Added empty project with architecture

---
+++ PlusMinusMobile/PlusMinusMobile/PlusMinusMobile/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using PlusMinusMobile.ViewModels;
+using PlusMinusMobile.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace PlusMinusMobile
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
